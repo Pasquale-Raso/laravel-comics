46d20589be0f18d64aecb0a64a530bf765c44d99
Pasquale-Raso <padqualeraso@gmail.com>
2021-10-02 13:41:41 +0200

    Stile ad ogni pagina tramite scss dedicati

--- resources/views/videos.blade.php
+++ resources/views/videos.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.template');
 @section('title')videos @endsection
 @section('Products')
-<h1>videos</h1>
-    <li><a href="{{url('/')}}">HOME</a></li>
+<div class="box ">
+    <div class="container">
+        <h1>videos</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod esse, qui rerum harum magni amet tempore hic quos incidunt, quam dolorem! Reprehenderit incidunt alias sequi repellat vero aperiam nisi. Rerum deserunt fugiat aut quis cupiditate eligendi nobis voluptatibus, beatae quasi eos temporibus quam modi aliquam consequuntur consectetur sunt. Tempora fuga sequi consequatur beatae qui. Voluptas sapiente illo, distinctio, voluptate natus porro magnam iusto laboriosam quisquam mollitia veritatis possimus molestiae fuga accusantium earum adipisci saepe dicta provident hic beatae doloribus eaque eveniet quis rerum! Exercitationem eum esse voluptates dolor quas ut quo labore velit tempore necessitatibus accusantium debitis praesentium voluptatem totam qui molestias corporis repudiandae laborum incidunt non, sunt nihil fugit. Quisquam illo magni fugit nam molestias, animi nostrum necessitatibus sit non magnam quas. Tempore aliquid molestiae doloremque explicabo, voluptates, cupiditate suscipit, ullam obcaecati possimus soluta unde! Quisquam nisi est commodi veniam sapiente, obcaecati consectetur necessitatibus expedita dignissimos deserunt autem laboriosam. Ullam quam omnis aspernatur natus corporis adipisci dolorem suscipit unde, exercitationem voluptatum distinctio dolor quis perferendis enim labore cupiditate voluptates velit est eaque quia quibusdam. Aliquam nisi necessitatibus excepturi suscipit a, aspernatur iste quidem ipsam recusandae veniam alias exercitationem? Eius ex hic rem laudantium cumque esse dolorum quis adipisci aliquid.</p>
+    </div>
+</div>
     @include('includes.navbaricons-white-line')
 @endsection
