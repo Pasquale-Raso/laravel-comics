ebf7fcaedbef7540c981ce8a17a3db0df52df65b
Pasquale-Raso <padqualeraso@gmail.com>
2021-10-04 21:19:48 +0200

    ex

--- resources/views/pages/fumetti/fumetti.blade.php
+++ resources/views/pages/fumetti/fumetti.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.template');
-@section('title')Comics-Detail @endsection
+@section('title')Fumetti @endsection
 @section('Products')
 <div class="box ">
     <div class="container">
-        <h1>Comics-Detail</h1>
+        <h1>{{$Fumetti ]['title']}}</h1>
+        <h1>Fumetti</h1>
+
     </div>
 </div>
     @include('includes.navbaricons-white-line')
